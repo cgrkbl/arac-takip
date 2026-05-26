d21fad411f201e5b3b379edccb8356f677d868c2
cgrkbl <cagrikabulbusiness@gmail.com>
2026-05-26 03:49:29 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+{
+  "name": "Küçüker Lojistik",
+  "short_name": "Küçüker Lojistik",
+  "description": "Küçüker Lojistik Araç Takip Sistemi",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#1c1c1e",
+  "theme_color": "#1c1c1e",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "192x192",
+      "type": "image/jpeg"
+    },
+    {
+      "src": "icon.png",
+      "sizes": "512x512",
+      "type": "image/jpeg"
+    }
+  ]
+}
