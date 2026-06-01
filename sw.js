ae078bb5f2097f0c7ec8592baba2edac4b02f1bb
cgrkbl <cagrikabulbusiness@gmail.com>
2026-06-01 11:23:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,8 @@
-const CACHE_NAME = 'kucuker-lojistik-v1';
-const URLS = ['/arac-takip/', '/arac-takip/index.html', '/arac-takip/manifest.json', '/arac-takip/icon.jpg'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS)));
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
-});
+ 
